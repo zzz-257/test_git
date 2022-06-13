af1c7f59e3c93b8c41afbaa50e85166ef95be2a1
Alreadydie <153080583@qq.com>
2022-06-13 14:39:10 +0800

    test_git init

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**********************************************************************************************
+* COPYRIGHT NOTICE
+* Copyright (c) 2020
+* All rights reserved
+* @author :chenjp
+* @file :/home/chenjianping/git/test_git\hello.c
+* @date :2022/06/13 10:11
+* @description :
+**********************************************************************************************/
+
+#include "hello.h"
+
+int main(int argc,char *argv[]){
+
+	fun();
+ return 0;
+}
+
---
+++ hello.h
@@ -0,0 +1,24 @@
+/**********************************************************************************************
+* COPYRIGHT NOTICE
+* Copyright (c) 2020
+* All rights reserved
+* @author :chenjp
+* @file :/home/chenjianping/git/test_git\hello.h
+* @date :2022/06/13 10:14
+* @description :
+**********************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+void fun(void)
+{
+	printf("hello git\n");
+}
+#endif
